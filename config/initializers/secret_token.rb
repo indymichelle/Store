eed98ce5ab0971a1ce160748c5d6fb9958fec9a7
Michelle Madden <maddenmichelle@yahoo.com>
2014-05-24 16:24:34 -0400

    Begining of Storefront application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = '9bdeb7cb9cc9891cdef00406deb3e8753f4c84502ca252e345cd001740d87c42347aaaebe38e382ba89e421afea616ae0d3b60f44df3f7b073b790f214de7045'
