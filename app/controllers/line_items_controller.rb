66ada446ac5151d0a883059d9163f057f792a38e
Michelle Madden <maddenmichelle@yahoo.com>
2014-07-06 13:56:55 -0400

    AJAX-Based cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,10 +28,10 @@ class LineItemsController < ApplicationController
   def create
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product.id)
-
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to shop_url }
+        format.js
         format.json { render action: 'show', status: :created, location: @line_item }
       else
         format.html { render action: 'new' }
